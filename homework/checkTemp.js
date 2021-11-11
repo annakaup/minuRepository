2411f4df5995b46b9013a5aece0329bd300e4b09
annakaup <92529075+annakaup@users.noreply.github.com>
2021-11-11 16:55:13 +0200

    Update checkTemp.js

--- homework/checkTemp.js
+++ homework/checkTemp.js
@@ -1,8 +1,11 @@
 function checkTemp(temp) {
-    if (temp == 20)
-    return 1
-    if (temp == 10)
-    return 0
-    if (temp == -5)
+    if (temp <= 20) {
     return -1
+    }
+    if (temp >=21 && temp <=40) {
+    return 0
+    }
+    if (temp >=41 && temp <=60){
+    return 1
+    }
 }
